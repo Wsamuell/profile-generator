b9ae6a07a893b3b09fe585ebcc9f6c0014604cc5
samuel Odubamowo <wemsamuel@gmail.com>
2021-05-23 03:34:15 -0700

    html included

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -1,27 +1,84 @@
 const HTMLTemplate = data => {
-    const { teamname, name, github, } = data
-    return
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../src/style.css">
-
-    <title>Team Profile Generator</title>
-</head>
-<body>
-    <header>
-        <h1> ${teamname} </h1>
-    </header>
-    <section>
-
-    </section>
+    const {name, role, id, email, officenumber, addNew, github, school } = data;
     
-</body>
-</html>
+    return
+
+// Header HTML     
+    `
+
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../src/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+        <title>Team Profile Generator</title>
+    </head>
+    
+    <body>
+        <header class="container-fluid">
+            <h1> My Team </h1>
+        </header>
+        <section class="container">
+            <article class="card-deck">
+            `
+// Using fs to write file into new HTML 
+fs.writeFile('generate.html', data, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+  
+// Creating each new employee based on chosen inputs
+            `
+
+    
+                <div class="card border-info mb-3" style="max-width: 18rem;">
+                    <div class="card-header"><br> Name <br></div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: </li>
+                            <li class="list-group-item">Email: </li>
+                            <li class="list-group-item">Office Number</li>
+                        </ul>
+                    </div>
+                </div>
+    
+                <div class="card bg-light mb-3" style="max-width: 18rem;">
+                    <div class="card-header"><br> Name <br></div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: </li>
+                            <li class="list-group-item">Email: </li>
+                            <li class="list-group-item">Github: </li>
+                        </ul>
+                    </div>
+                </div>
+    
+                <div class="card border-info mb-3" style="max-width: 18rem;">
+                    <div class="card-header"><br> Name <br></div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: </li>
+                            <li class="list-group-item">Email: </li>
+                            <li class="list-group-item">School: </li>
+                        </ul>
+                    </div>
+                </div>
+    
+            </article>
+        </section>
+    
+    </body>
+    
+    </html>
 
 `}
 module.exports = HTMLTemplate
--- index.js
+++ index.js
@@ -1,116 +1,32 @@
 // other files being read from projects
 const Employee = require('./lib/Employee');
 
-const Questions = require ('./lib/Questions');
+const Questions = require('./lib/Questions');
 const Manager = require('./lib/Manager');
 const intern = require('./lib/Intern');
 const HTMLTemplate = require('./dist/generateHTML');
 
 // installed packages
 const inquirer = require('inquirer');
-const fs = require ('fs');
+const fs = require('fs');
+const questions = require('./lib/Questions');
+
+function startQuestion() {
+    inquirer.prompt(questions)
+    // *********
+        .then(HTMLTemplate({name, role, id, email, officenumber, addNew, github, school}) {
+
+        })
+
+}
 
 // starting prompt
 function writePrompt() {
     console.log("is this working")
-    questions;
+    startQuestion();
 };
 
-// the questions to be prompted .... ***DON'T FORGET TO MOVE THIS TO QUESTIONS.JS***
-function questions() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "teamname",
-            message: "Please enter the name of the team!",
-            validate: (value) => {
-                if (value) { return true }
-                else { return "Team name Required" }
-            }
-        },
-        {
-            type: "input",
-            name: "name",
-            message: "Please enter Employee's Full Name!",
-            validate: (value) => {
-                if (value) { return true }
-                else { return "Role Required" }
-            }
-        },
-        {
-            type: "list",
-            name: "role",
-            message: `Please enter role!`,
-            choices: [
-                "Engineer",
-                "Manager",
-                "Intern",
-            ],
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Please enter Employee's ID!",
-            validate: (value) => {
-                if (value) { return true }
-                else { return "ID Required" }
-            }
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter Employee email?",
-        }
-    ])
-        // questions to add role depending on the choice selected
-        .then(function ({ name, role, id, email }) {
-            inquirer.prompt([
-                {
-                    type: "input",
-                    name: "officenumber",
-                    message: "Please enter office number",
-                    when:  ({role}) => role === "Manager",
-                    validate: (value) => {
-                        if (value) { return true }
-                        else { return "Office number Required" }
-                    }
-                },
-                {
-                    type: "input",
-                    name: "officenumber",
-                    message: "Please enter office number",
-                    when:  ({role}) => role === "Manager",
-                    validate: (value) => {
-                        if (value) { return true }
-                        else { return "Office number Required" }
-                    }
-                },
-                {
-                    type: "input",
-                    name: "github",
-                    message: "Please enter Github Username",
-                    validate: (value) => {
-                        if (value) { return true }
-                        else { return "Github Username Required" }
-                    }
-                }, {
-                    type: "input",
-                    name: "github",
-                    message: "Please enter Github Username",
-                    validate: (value) => {
-                        if (value) { return true }
-                        else { return "Github Username Required" }
-                    }
-                }
-
-
-            ])
-        })
-
-};
-
-
-writePrompt;
+writePrompt();
 
 
 
